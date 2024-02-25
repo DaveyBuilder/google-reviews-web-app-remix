869c9493ac0a906c8eacc725d045992f5177b7ed
david586 <d.harding1020@gmail.com>
2024-02-25 21:00:19 +0700

    PWA enhancements

--- app/root.jsx
+++ app/root.jsx
@@ -22,7 +22,12 @@ export default function App() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
-        <link rel="icon" href="/star.png" />
+        <link rel="icon" href="/star64.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/star256white.png" sizes="256x256" />
+        <link rel="apple-touch-icon" href="/star512white.png" sizes="512x512" />
+        <meta name="apple-mobile-web-app-title" content="Google Reviews"></meta>
+        <meta name="apple-mobile-web-app-capable" content="yes"></meta>
       </head>
       <body>
         <Outlet />
